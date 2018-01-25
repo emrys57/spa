07104c34bbf0e27364b3398ae3285c3d20abb3a7
emrys57 <e.williams@linkz-im.com>
2018-01-25 14:17:30 +0000

    Committed after completing chapter 3

---
+++ js/spa.js
@@ -0,0 +1,26 @@
+/*
+ * spa.js
+ * Root namespace module
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true,  indent : 2,     maxerr  : 50,
+  newcap : true,   nomen : true, plusplus : true,
+  regexp : true,  sloppy : true,     vars : false,
+  white  : true
+*/
+/*global $, spa */
+
+/*
+ * spa.js
+ * Root namespace module
+*/
+
+/*global $, spa */
+var spa = (function () {
+  var initModule = function ( $container ) {
+     spa.shell.initModule( $container );
+  };
+
+  return { initModule: initModule };
+}());
